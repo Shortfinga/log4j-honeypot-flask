5f0823df70562811bcb603fa3ae62243864a609f
wirehack7 <markusfritz87@gmail.com>
2021-12-19 14:35:14 +0100

    switched back to only pprint

--- app/app.py
+++ app/app.py
@@ -125,10 +125,10 @@ def reportHit(request):
         for header in request.headers:
             report[header[0]] = str(header[1])
         if request.form.items():
-            cdict = {request.form.items().name: request.form.items().value for c in request.form.items()}
+            #cdict = {request.form.items().name: request.form.items().value for c in request.form.items()}
             #['fields'] = request.form.items()
-            #for fieldname, value in request.form.items():
-            #    report[str(fieldname)] = report[str(value)]
+            for fieldname, value in request.form.items():
+                report[fieldname] = str(value)
         report['src_ip'] = request.remote_addr
         report['timestamp'] = datetime.now().isoformat()
         report['sensor'] = config['DEFAULT']['name']
